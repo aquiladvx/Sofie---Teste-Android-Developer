988f81ba73962073667fd0b066faf48e657d8944
Davi Aquila <daviaquila162@gmail.com>
2021-01-06 17:17:12 -0300

    Init test

---
+++ app/src/androidTest/java/com/debcomp/aql/sofietesteandroiddeveloper/TaskTest.kt
@@ -0,0 +1,30 @@
+package com.debcomp.aql.sofietesteandroiddeveloper
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+
+/*
+ * Davi Áquila
+ * aquiladvx
+ *
+ * 06/01/2021
+ *
+ */
+
+@RunWith(AndroidJUnit4::class)
+class TaskTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    /*
+        GIVEN
+
+        WHEN
+
+        THEN
+     */
+}
